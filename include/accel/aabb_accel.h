57d09be011ac1c097706701c701797e1b5a0c645
ippqw5 <ippqw5@163.com>
2026-09-05 17:00:32 +0800

    refactor: remove old AABB implementation and integrate into aabb_accel

--- include/accel/aabb_accel.h
+++ include/accel/aabb_accel.h
@@ -1,11 +1,25 @@
 #ifndef AABB_ACCEL_H
 #define AABB_ACCEL_H
 
-#include "aabb.h"
 #include "accel.h"
 
 namespace tcpr
 {
+
+/**
+ * Axis-aligned bounding box for ray-shape intersection acceleration.
+ * Default-constructed box is empty (min = +inf, max = -inf).
+ */
+struct AABB
+{
+    void expand(const glm::vec3& point);
+
+    // Slab-method ray-box test within `[t_min, t_max]`.
+    [[nodiscard]] bool intersect(const Ray& ray, float t_min, float t_max) const;
+
+    glm::vec3 min{std::numeric_limits<float>::infinity()};
+    glm::vec3 max{-std::numeric_limits<float>::infinity()};
+};
 
 /**
  * Reference acceleration structure: a single AABB over all triangles,
